f09801d4221d9a0adac0443717769ab41c89d4cb
suinua <maatinosippo@gmail.com>
2021-05-26 23:00:00 +0900

    fix

--- src/copyanni/model/job/Thor.php
+++ src/copyanni/model/job/Thor.php
@@ -6,14 +6,23 @@ namespace copyanni\model\job;
 
 use copyanni\item\FireStorm;
 use copyanni\item\Hammer;
+use copyanni\service\SoundService;
 use game_chef\api\GameChef;
 use pocketmine\entity\Effect;
 use pocketmine\entity\EffectInstance;
+use pocketmine\entity\Entity;
+use pocketmine\entity\EntityIds;
+use pocketmine\event\entity\EntityDamageByEntityEvent;
+use pocketmine\event\entity\EntityDamageEvent;
 use pocketmine\item\enchantment\Enchantment;
 use pocketmine\item\enchantment\EnchantmentInstance;
 use pocketmine\item\Item;
 use pocketmine\item\ItemIds;
+use pocketmine\math\Vector3;
+use pocketmine\network\mcpe\protocol\AddActorPacket;
+use pocketmine\network\mcpe\protocol\AddEntityPacket;
 use pocketmine\Player;
+use pocketmine\Server;
 
 class Thor extends Job
 {
@@ -41,7 +50,24 @@ class Thor extends Job
 
         $playerData = GameChef::findPlayerData($player->getName());
         $targets = GameChef::getAroundEnemies($playerData->getBelongGameId(), [$playerData->getBelongTeamId()], $player, 4);
-        foreach ($targets as $target) $target->setOnFire(6);
+        foreach ($targets as $target) {
+            $light = new AddActorPacket();
+            $light->type = "minecraft:lightning_bolt";
+            $light->entityRuntimeId = Entity::$entityCount++;
+            $light->metadata = [];
+            $light->motion = null;
+            $light->yaw = $player->getYaw();
+            $light->pitch = $player->getPitch();
+            $light->position = new Vector3($player->getX(), $player->getY(), $player->getZ());
+            Server::getInstance()->broadcastPacket([$target, $player], $light);
+
+            //eventはcallしなくていい
+            $target->setHealth($target->getHealth() - 5);
+            $target->setLastDamageCause(new EntityDamageByEntityEvent($player, $target, EntityDamageEvent::CAUSE_MAGIC, 5));
+
+            SoundService::play($target, $target, "ambient.weather.thunder", 1, 1);
+            SoundService::play($player, $target, "ambient.weather.thunder", 1, 1);
+        }
         return $result;
     }
 }
